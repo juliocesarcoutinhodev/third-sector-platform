a45193e5debc504bd5365445f00ad7eb4a969be4
Julio Cesar Coutinho <juliocesar.coutinhodev@outlook.com>
2026-06-19 10:44:22 -0300

    Initial Project

---
+++ third-sector-api/src/main/java/br/com/toponesystem/thirdsectorapi/ThirdSectorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.toponesystem.thirdsectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdSectorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdSectorApiApplication.class, args);
+    }
+
+}
